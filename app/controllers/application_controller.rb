4de0626c654189041693de7bb1bc95fd3ec2126b
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2015-11-21 13:37:13 +0000

    Added CORS settings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+   after_action :access_control_headers
 
   def render_json(body, status_code = :ok)
     render json: body, status: status_code
@@ -7,5 +8,10 @@ class ApplicationController < ActionController::API
   def render_error(message, status_code = :unprocessable_entity)
     render_json(message, status_code)
   end
+
+  def set_access_control_headers
+     headers['Access-Control-Allow-Origin']   = "*"
+     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+   end
 
 end
