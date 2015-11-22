6a02b5d0e8863e6f0dc06487c928a653328581f4
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2015-11-22 13:28:33 +0000

    Updated seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-user1 = User.create(email: "trackhack@gmail.com", password: "123456", amount: 3317.63)
+user1 = User.create(email: "trackhack@gmail.com", password: "123123", amount: 0.63)
 user2 = User.create(email: "sebastian@gmail.com", password: "123123", amount: 100)
 
 Tag.create(name: "11001214", user: user1)
